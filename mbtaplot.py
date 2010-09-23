c5f4afa8130f0d64a6ce37e4ce62a9d9d8223bfa
Jeff Kaufman <jeff@alum.swarthmore.edu>
2010-09-23 09:22:22 -0400

    ajaxified

--- mbtaplot.py
+++ mbtaplot.py
@@ -2,11 +2,13 @@ from __future__ import with_statement
 import os
 import urllib2
 import time
+import cgi
 import xml.dom.minidom as minidom
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from xml.sax.saxutils import escape
+import simplejson as json
 
 maps_key='ABQIAAAA5EZP5qCoaub5KG1QSmw1KhQQgZrvOX3gye-SKpRLyNsCw8EkTRTsLF90-utHavKb_PVOhaSa31S44Q'
 
@@ -14,18 +16,19 @@ BUS_FEED="http://webservices.nextbus.com/service/publicXMLFeed?"
 
 class Bus(object):
     def __init__(self, xml_vehicle):
+        self.attrs = {}
         for attribute in ("dirTag", "heading", "id", "lat", "lon", "routeTag", "secsSinceReport"):
-            setattr(self, attribute, xml_vehicle.getAttribute(attribute))
-
+            self.attrs[attribute] = xml_vehicle.getAttribute(attribute)
+        self.attrs["roundHeading"] = self.round_heading
+        
     @property
     def round_heading(self):
-        return (int(int(self.heading)/3)*3)%120
+        return (int(int(self.attrs["heading"])/3)*3)%120
 
 class BusRoute(object):
     def __init__(self, route_num):
         self.route_num = route_num
 
-    @property
     def paths(self):
         use_url = BUS_FEED + "&".join(("command=routeConfig",
                                        "a=mbta",
@@ -37,8 +40,7 @@ class BusRoute(object):
 
         return [Path(p) for p in xmldoc.getElementsByTagName("path")]
 
-    @property
-    def busses(self):        
+    def buses(self):        
         use_url = BUS_FEED + "&".join(("command=vehicleLocations",
                                        "a=mbta",
                                        "r=%s" % self.route_num,
@@ -52,6 +54,11 @@ class BusRoute(object):
         
         return [Bus(vehicle) for vehicle in xmldoc.getElementsByTagName("vehicle")]
 
+    @staticmethod
+    def age_buses(buses, time_diff):
+        for bus in buses:
+            bus.attrs["secsSinceReport"] = int(bus.attrs["secsSinceReport"]) + time_diff
+
 class Point(object):
     def __init__(self, xml_point):
         self.lat = xml_point.getAttribute("lat")
@@ -63,19 +70,43 @@ class Path(object):
     def __getitem__(self, x):
         return self.points[x]
 
+class Paths(webapp.RequestHandler):
+    def get(self):
+        paths = BusRoute(cgi.escape(self.request.get('route'))).paths()
+        self.response.out.write(json.dumps([[{"lat": point.lat,
+                                              "lng": point.lon}
+                                             for point in path]
+                                            for path in paths]))
+        
+class Buses(webapp.RequestHandler):
+    cache = {}
+    def buses_for_route(self, route):
+        if route in self.cache:
+            timestamp, buses = self.cache[route]
+            time_diff = int(time.time() - timestamp)
+            if time_diff < 45:
+                BusRoute.age_buses(buses, time_diff)
+                return buses
+        buses = BusRoute(route).buses()
+        self.cache[route] = (time.time(), buses)
+        return buses
+
+    def get(self):
+        route = cgi.escape(self.request.get('route'))
+        buses = self.buses_for_route(route)
+        self.response.out.write(json.dumps([bus.attrs for bus in buses]))
+
 class MainPage(webapp.RequestHandler):
     def get(self):
-        route = BusRoute("77")
-        template_values = {"busses": route.busses,
-                           "paths": route.paths}
+        template_values = {"route": "77"}
         
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                     ],
-                                     debug=True)
+application = webapp.WSGIApplication([('/', MainPage), 
+                                      ('/Paths', Paths),
+                                      ('/Buses', Buses),
+                                     ], debug=True)
 
 def main():
     run_wsgi_app(application)
