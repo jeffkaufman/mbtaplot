b5846ddba1361604d5237f3ac15bb37f65dd593b
Jeff Kaufman <jeff@alum.swarthmore.edu>
2010-09-25 17:02:59 -0400

    now snap to routes and add checkboxes for configuring

--- mbtaplot.py
+++ mbtaplot.py
@@ -1,4 +1,5 @@
 from __future__ import with_statement
+import sys
 import os
 import urllib2
 import time
@@ -19,6 +20,9 @@ class Bus(object):
         self.t = time.time() - int(self.secsSinceReport)
         del self.secsSinceReport
 
+        self.lat = float(self.lat)
+        self.lon = float(self.lon)
+
         self.oldT = self.t
         self.oldLat = self.lat
         self.oldLon = self.lon
@@ -36,8 +40,6 @@ class Bus(object):
         return (int(int(self.heading)/3)*3)%120
 
     def estimate_pos(self, l_i, l_j):
-        l_i, l_j = float(l_i), float(l_j)
-
         t_i = self.oldT
         t_j = self.t
         t_k = time.time()
@@ -47,7 +49,7 @@ class Bus(object):
 
         delta_t = t_j - t_i
         delta_l = l_j - l_i
-        
+
         l_k = l_j + (t_k-t_j)*(l_j-l_i)/(t_j-t_i)
 
         return l_k
@@ -60,95 +62,149 @@ class Bus(object):
     def elon(self):
         return self.estimate_pos(self.oldLon, self.lon)
 
-    def sendable(self):
+    def sendable(self, snap, estimate):
+        if estimate:
+            lat, lon = self.elat, self.elon
+        else:
+            lat, lon = self.lat, self.lon
+
+        if snap:
+            lat, lon = snap_to_path(self.routeTag, lat, lon)
+
         return {
-            "elat": self.elat,
-            "elon": self.elon,
+            "elat": lat,
+            "elon": lon,
             "id": self.id,
             "age": int(self.age),
             "rhead": self.round_heading,
-            
-            "lat": self.lat,
-            "lon": self.lon,
-            
-            "oldLat": self.oldLat,
-            "oldLon": self.oldLon,
-
-            "t": self.t,
-            "oldT": self.oldT,
             }
 
-class BusRoute(object):
-    def __init__(self, route_num):
-        self.route_num = route_num
+def distance(x1,y1,x2,y2):
+    return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
 
-    def paths(self):
+def closest_point_on_line(line, x3, y3):
+    """ from http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/ """
+    x1, y1, x2, y2 = line
+
+    u = ((x3-x1)*(x2-x1) + (y3-y1)*(y2-y1)) / distance(*line)
+
+    if u < 0 or u > 1:
+        """ use one of the ends """
+        if distance(x1,y1,x3,y3) > distance(x2,y2,x3,y3):
+            return x2,y2
+        else:
+            return x1,y1
+    else:
+        """ use a point on the line """
+
+        x4 = x1 + u*(x2 - x1)
+        y4 = y1 + u*(y2 - y1)
+        
+        return x4, y4
+
+def distance_to_line(line, x3, y3):
+    """ from http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/ """
+    x1, y1, x2, y2 = line
+
+    x4, y4 = closest_point_on_line(line, x3, y3)
+    return distance(x4, y4, x3, y3)
+
+
+def snap_to_path(route, lat, lon):
+    best_line = None
+    best_dist = None
+
+    for path in request_paths(route):
+
+        for p_i, p_j in zip(path[:-1], path[1:]):
+            line = p_i.lat, p_i.lon, p_j.lat, p_j.lon
+
+            dist = distance_to_line(line, lat, lon)
+            if best_line == None or dist < best_dist:
+                best_line = line
+                best_dist = dist
+
+    if best_line is None or best_dist > .00001:
+        """ if we failed to find a good line, or if we'd be adjusting too far, do nothing """
+        return lat, lon
+
+    return closest_point_on_line(best_line, lat, lon)
+
+
+def request_paths(route_num, path_cache={}):
+    # path cache shared between calls
+    # never updates path cache
+
+    if route_num not in path_cache:
         use_url = BUS_FEED + "&".join(("command=routeConfig",
                                        "a=mbta",
-                                       "r=%s" % self.route_num
+                                       "r=%s" % route_num
                                        ))
         usock = urllib2.urlopen(use_url)
         xmldoc = minidom.parse(usock)
         usock.close()
 
-        return [Path(p) for p in xmldoc.getElementsByTagName("path")]
+        path_cache[route_num] = [Path(p) for p in xmldoc.getElementsByTagName("path")]
+    return path_cache[route_num]
 
-    def buses(self):
-        use_url = BUS_FEED + "&".join(("command=vehicleLocations",
-                                       "a=mbta",
-                                       "r=%s" % self.route_num,
-                                       #"t=%s" % int(time.time())
-                                       "t=0"
-                                       ))
+def request_buses(route_num):
+    use_url = BUS_FEED + "&".join(("command=vehicleLocations",
+                                   "a=mbta",
+                                   "r=%s" % route_num,
+                                   #"t=%s" % int(time.time())
+                                   "t=0"
+                                   ))
 
-        usock = urllib2.urlopen(use_url)
-        xmldoc = minidom.parse(usock)
-        usock.close()
+    usock = urllib2.urlopen(use_url)
+    xmldoc = minidom.parse(usock)
+    usock.close()
 
-        bus_hash = {}
-        for vehicle in xmldoc.getElementsByTagName("vehicle"):
-            bus = Bus(vehicle)
-            bus_hash[bus.id] = bus
+    bus_hash = {}
+    for vehicle in xmldoc.getElementsByTagName("vehicle"):
+        bus = Bus(vehicle)
+        bus_hash[bus.id] = bus
 
-        return bus_hash
+    return bus_hash
 
-    @staticmethod
-    def update_buses(old_buses, new_buses):
-        for bus_id, new_bus in new_buses.items():
-            try:
-                old_bus = old_buses[new_bus.id]
-            except KeyError:
-                continue
 
-            if old_bus.age - new_bus.age < 5:
-                """ if the new bus is not at least 5sec more recently
-                    updated than the old bus, don't update previous
-                    position and report time """
-                new_bus.oldT = old_bus.oldT
-                new_bus.oldLat = old_bus.oldLat
-                new_bus.oldLon = old_bus.oldLon
-            else:
-                new_bus.oldT = old_bus.t
-                new_bus.oldLat = old_bus.lat
-                new_bus.oldLon = old_bus.lon
+def update_buses(old_buses, new_buses):
+    for bus_id, new_bus in new_buses.items():
+        try:
+            old_bus = old_buses[new_bus.id]
+        except KeyError:
+            continue
 
-        return new_buses
+        if old_bus.age - new_bus.age < 5:
+            """ if the new bus is not at least 5sec more recently
+            updated than the old bus, don't update previous
+            position and report time """
+            new_bus.oldT = old_bus.oldT
+            new_bus.oldLat = old_bus.oldLat
+            new_bus.oldLon = old_bus.oldLon
+        else:
+            new_bus.oldT = old_bus.t
+            new_bus.oldLat = old_bus.lat
+            new_bus.oldLon = old_bus.lon
 
-    @staticmethod
-    def allRoutes():
-        use_url = BUS_FEED + "&".join(("command=routeList",
+    return new_buses
+
+
+def allRoutes():
+    use_url = BUS_FEED + "&".join(("command=routeList",
                                        "a=mbta"))
-        usock = urllib2.urlopen(use_url)
-        xmldoc = minidom.parse(usock)
-        usock.close()
+    usock = urllib2.urlopen(use_url)
+    xmldoc = minidom.parse(usock)
+    usock.close()
 
-        return [BusRoute(route.getAttribute("title")) 
-                for route in xmldoc.getElementsByTagName("route")]
+    return [BusRoute(route.getAttribute("title"))
+            for route in xmldoc.getElementsByTagName("route")]
 
 class Point(object):
     def __init__(self, xml_point):
-        self.lat = xml_point.getAttribute("lat")
-        self.lon = xml_point.getAttribute("lon")
+        self.lat = float(xml_point.getAttribute("lat"))
+        self.lon = float(xml_point.getAttribute("lon"))
+    def __repr__(self):
+        return "(%s, %s)" % (self.lat, self.lon)
 
 class Path(object):
     def __init__(self, xml_path):
@@ -165,13 +221,13 @@ class Paths(webapp.RequestHandler):
             self.cache[route] = json.dumps([[{"lat": point.lat,
                                               "lng": point.lon}
                                              for point in path]
-                                            for path in BusRoute(route).paths()])
+                                            for path in request_paths(route)])
         self.response.out.write(self.cache[route])
 
 
 class Routes(webapp.RequestHandler):
     def get(self):
-        routes = BusRoute.allRoutes()
+        routes = allRoutes()
         self.response.out.write(json.dumps([route.route_num for route in routes]))
 
 
@@ -193,14 +249,20 @@ class Buses(webapp.RequestHandler):
 
     def get(self):
         route = cgi.escape(self.request.get('route'))
+        
+        snap = cgi.escape(self.request.get('snap')).lower() != "false"
+        est = cgi.escape(self.request.get('est')).lower() != "false"
+
         now = time.time()
         if now - self.timestamp(route) > 12:
-            new_buses = BusRoute.update_buses(self.buses(route), 
-                                              BusRoute(route).buses())
+            """ refresh buses from server every 10sec """
+
+            new_buses = update_buses(self.buses(route),
+                                     request_buses(route))
             self.cache[route] = now, new_buses
 
         self.response.out.write(json.dumps(
-                [bus.sendable() for bus in self.buses(route).values()]))
+                [bus.sendable(snap, est) for bus in self.buses(route).values()]))
 
 
 class MainPage(webapp.RequestHandler):
@@ -208,6 +270,10 @@ class MainPage(webapp.RequestHandler):
 
         buses = cgi.escape(self.request.get('buses')).lower() == "true"
         shading = cgi.escape(self.request.get('shading')).lower() == "true"
+
+        snap = cgi.escape(self.request.get('snap')).lower() != "false"
+        est = cgi.escape(self.request.get('est')).lower() != "false"
+
         all_routes = False
 
         routes = []
@@ -222,6 +288,8 @@ class MainPage(webapp.RequestHandler):
 
         template_values = {"shading": shading,
                            "all_routes": all_routes,
+                           "snap": snap,
+                           "est": est,
                            "buses": buses,
                            "routes": routes}
 
